604e49521a79d8a9d976ea44d7369004fc5e4e42
qiyan7 <>
2019-10-31 22:24:28 +0800

    no message

--- src/components/plTable.vue
+++ src/components/plTable.vue
@@ -12,7 +12,7 @@
         :border="border"
         :stripe="stripe"
         :row-key="rowKey"
-        :row-style="rowStyle"
+        :row-style="rowStyle1"
         :empty-text="emptyText"
         :show-header="showHeader"
         :addfence="addfence"
@@ -92,13 +92,13 @@
 </template>
 
 <script type="text/babel">
-import draggable from "vuedraggable";
-import ResizeObserver from "resize-observer-polyfill";
-import ElTable from "pl-table/package/table";
-import plTableColumn from "pl-table/package/table-column";
-import ElPagination from "pl-table/package/pagination";
+import draggable from 'vuedraggable';
+import ResizeObserver from 'resize-observer-polyfill';
+import ElTable from 'pl-table/package/table';
+import plTableColumn from 'pl-table/package/table-column';
+import ElPagination from 'pl-table/package/pagination';
 export default {
-  name: "PlTable",
+  name: 'PlTable',
   components: { draggable, ElTable, ElPagination, plTableColumn },
   props: {
     heightSwitch: { type: Boolean, default: true }, // 是否需要去计算表格高度，关闭就相当于自适应高度，超出表格高度不会出现滚动条，开启相当于会去计算外层高度，大于表格高度，超出高度表格出现滚动条 (注意为false时候，请不要设置useVirtual属性)
@@ -107,16 +107,16 @@ export default {
     datas: { type: Array, default: () => [] }, // 表格数据
     dialogData: { type: Array, default: () => [] }, // 选择显示字段数组
     stripe: { default: false, type: Boolean }, // 是否为斑马纹
-    size: { default: "", type: String }, // Table 的尺寸
+    size: { default: '', type: String }, // Table 的尺寸
     showHeader: { default: true, type: Boolean }, // 是否显示表头
-    emptyText: { default: "暂无数据", type: String }, // 空数据时显示的文本内容
+    emptyText: { default: '暂无数据', type: String }, // 空数据时显示的文本内容
     border: { default: true, type: Boolean }, // 是否显示纵向边框
     paginationShow: { default: true, type: Boolean }, // 是否需要分页器 默认是true
     showSummary: { default: false, type: Boolean }, // 是否需要合计
     defaultExpandAll: { default: false, type: Boolean }, // 是否默认展开所有行，当 Table 包含展开行存在或者为树形表格时有效
     treeProps: {
       default() {
-        return { hasChildren: "hasChildren", children: "children" };
+        return { hasChildren: 'hasChildren', children: 'children' };
       },
       type: Object
     }, // 渲染嵌套数据的配置选项
@@ -126,17 +126,17 @@ export default {
     currentPage: { default: 1, type: Number }, // 当前页
     pageSizes: { default: () => [10, 20, 30, 50], type: Array }, // 每页显示个数选择器的选项设置
     totalOption: { default: () => [], type: Array }, // 需要合计的选项
-    sumText: { default: "合计", type: String }, // 合计行名称
+    sumText: { default: '合计', type: String }, // 合计行名称
     paginationLayout: {
-      default: "total, sizes, prev, pager, next, jumper",
+      default: 'total, sizes, prev, pager, next, jumper',
       type: String
     }, // 分页组件布局，子组件名用逗号分隔
     fieldSort: { default: true, type: Boolean }, // 字段排序
-    rowKey: { default: "", type: String }, // 支持树类型的数据。此时，必须要指定 row-key (注意，当开启useVirtual时，无效)
+    rowKey: { default: '', type: String }, // 支持树类型的数据。此时，必须要指定 row-key (注意，当开启useVirtual时，无效)
     highlightCurrentRow: { default: true, type: Boolean }, // 是否要高亮当前行
     showAmend: { default: false, type: Boolean }, // 是否显示修改字段名按钮(侧滑框)
-    amendBtnIcon: { default: "", type: String }, // 修改字段按钮的图标(侧滑框)
-    fieldTitle: { default: "选择显示字段", type: String }, // 弹框的标题(侧滑框)
+    amendBtnIcon: { default: '', type: String }, // 修改字段按钮的图标(侧滑框)
+    fieldTitle: { default: '选择显示字段', type: String }, // 弹框的标题(侧滑框)
     recordTableSelect: { default: false, type: Boolean }, // 是否记录表格的选项id(必须保证行ID存在，且唯一)
     headerDragStyle: { default: false, type: Boolean }, // 是否修改表格的头部拖动样式  | Boolean | false
     headerDragShow: { default: true, type: Boolean }, // 在配置了header-drag-style属性为true情况下， 如果你想列不可被拖动时，表头列右边框线依然显示
@@ -145,8 +145,8 @@ export default {
     excessRows: { default: 3, type: Number }, //	可视区域上方和下方额外渲染的行数，行数越多表格接替渲染效果越好，但越耗性能	Number	3
     pagingScroll: { default: true, type: Boolean }, // 跳转分页是否需要把表体滚动条回到顶部及左侧
     spanMethod: Function, // 合并行或列的计算方法 (注意，当开启useVirtual时，无效)
-    tooltipEffect: { default: "dark", type: String }, // tooltip effect 属性	String	dark/light
-    tooltipPlacement: { default: "top", type: String }, // Tooltip 的出现位置	String	top
+    tooltipEffect: { default: 'dark', type: String }, // tooltip effect 属性	String	dark/light
+    tooltipPlacement: { default: 'top', type: String }, // Tooltip 的出现位置	String	top
     summaryMethod: Function, // 自定义的合计计算方法
     addfence: { default: false, type: Boolean }, // 是否需要在表格尾部添加一行计算表格围栏。（比如你觉得合计一行不够，你还需要求平均值，或者其他）默认为帮你求平均值 | 如果你要自定义 请看fenceMethod属性
     fenceMethod: Function, // 自定义新添加计算围栏的计算方法（类似自定义合计算方法一样）
@@ -158,25 +158,25 @@ export default {
   },
   data() {
     return {
-      _times: "", // 弹框定时器
+      _times: '', // 弹框定时器
       plDialogFals: false, // 侧滑弹框是否开启
       newDialogData: [], // 新自定义字段的数据
-      maxHeight: "0", // 表格最大高度
-      aBox: "", // 蒙层节点
-      newPageSize: "", // 每页条数
-      newcurrentPage: "", // 当前页
+      maxHeight: '0', // 表格最大高度
+      aBox: '', // 蒙层节点
+      newPageSize: '', // 每页条数
+      newcurrentPage: '', // 当前页
       rowSelectionsData: [], // 当前选中行的数据(用于数据选中回显)  // 必须有type属性为selection
       tableSelectData: [], // 整个表格勾选项（含分页）
       cancelSelectData: [], // 被取消选中的ID数组
       arraySpanMethodFals: false, // 开启合并列条件判断
-      newTableStore: "" // 表格的store
+      newTableStore: '' // 表格的store
     };
   },
   created() {
     this.newPageSize = this.pageSize;
     this.newcurrentPage = this.currentPage;
     // 是否有条件开启合并列
-    if (typeof this.spanMethod === "function") {
+    if (typeof this.spanMethod === 'function') {
       this.arraySpanMethodFals = true;
     }
   },
@@ -193,9 +193,12 @@ export default {
     ro2.observe(this.$refs.tableBox);
   },
   methods: {
+    rowStyle1({ row }) {
+      console.log(row);
+    },
     // 自定义新添加计算围栏的方法
     fenceMethods(param) {
-      if (typeof this.fenceMethod === "function" && this.fenceMethod) {
+      if (typeof this.fenceMethod === 'function' && this.fenceMethod) {
         return this.fenceMethod(param);
       }
     },
@@ -219,7 +222,7 @@ export default {
           return self.indexOf(element) === index;
         });
       } else {
-        throw new Error("需要的是数组");
+        throw new Error('需要的是数组');
       }
     },
     // 数组对象去重的方法
@@ -235,12 +238,12 @@ export default {
         }
         return result;
       } else {
-        throw new Error("需要的是数组");
+        throw new Error('需要的是数组');
       }
     },
     // 当用户手动勾选全选 Checkbox 时触发的事件	selection
     selectAll(selection) {
-      this.$emit("select-all", selection);
+      this.$emit('select-all', selection);
       // 当用户取消全选，就去找出表格当前的数据ID,然后去删除记录选择项ID数组对应的值
       if (this.recordTableSelect) {
         if (selection.length === 0) {
@@ -266,7 +269,7 @@ export default {
         }
         // 这就是整个表格当前选中项（包含分页）
         this.$emit(
-          "table-select-change",
+          'table-select-change',
           this.tableSelectData,
           this.cancelSelectData
         );
@@ -274,7 +277,7 @@ export default {
     },
     // 当用户手动勾选数据行的 Checkbox 时触发的事件
     plSelect(selection, row) {
-      this.$emit("select", selection, row);
+      this.$emit('select', selection, row);
       // （执行删除选中节点操作）
       if (row.id && this.recordTableSelect) {
         // 思路如下：  selection表示当前表格选中的节点数组, row表示当前点击的行节点
@@ -303,7 +306,7 @@ export default {
         }
         // 这就是整个表格当前选中项（包含分页）
         this.$emit(
-          "table-select-change",
+          'table-select-change',
           this.tableSelectData,
           this.cancelSelectData
         );
@@ -311,7 +314,7 @@ export default {
     },
     // 当选择项发生变化时会触发该事件
     handleSelectionChange(val) {
-      this.$emit("selection-change", val);
+      this.$emit('selection-change', val);
       if (this.recordTableSelect) {
         //（做添加选中节点操作）
         val.forEach(value => {
@@ -323,7 +326,7 @@ export default {
         );
         // 这就是整个表格当前选中项（包含分页）
         this.$emit(
-          "table-select-change",
+          'table-select-change',
           this.tableSelectData,
           this.cancelSelectData
         );
@@ -335,7 +338,7 @@ export default {
         // 需要记录选中项id
         if (this.recordTableSelect) {
           // 去重过后的值
-          let datasFiler = this.removalDataObj(rows, "id");
+          let datasFiler = this.removalDataObj(rows, 'id');
           // 筛选过滤数组
           function filterFun(data, isd) {
             return data.filter(item => item.row.id !== isd);
@@ -350,7 +353,7 @@ export default {
         }
         this.newtoggleRowSelections();
       } else {
-        console.error("数据格式需要一个数组");
+        console.error('数据格式需要一个数组');
       }
     },
     // 用于多选表格，切换某一行的选中状态
@@ -372,7 +375,7 @@ export default {
       if (this.$refs.singleTable) {
         this.$refs.singleTable.toggleAllSelection();
       } else {
-        console.error("clearSelection方法为找到，可能表格未加载完毕");
+        console.error('clearSelection方法为找到，可能表格未加载完毕');
       }
     },
     // 用于多选表格，清空用户的选择
@@ -380,13 +383,13 @@ export default {
       if (this.$refs.singleTable) {
         this.$refs.singleTable.clearSelection();
       } else {
-        console.error("clearSelection方法为找到，可能表格未加载完毕");
+        console.error('clearSelection方法为找到，可能表格未加载完毕');
       }
     },
     // 分页器的事件
     handleSizeChange(val) {
       this.newPageSize = val;
-      this.$emit("handlePageSize", {
+      this.$emit('handlePageSize', {
         size: this.newPageSize,
         page: this.newcurrentPage
       });
@@ -397,7 +400,7 @@ export default {
         this.$refs.singleTable.pagingScrollTop(0);
       }
       this.newcurrentPage = val;
-      this.$emit("handlePageSize", {
+      this.$emit('handlePageSize', {
         size: this.newPageSize,
         page: this.newcurrentPage
       });
@@ -408,7 +411,7 @@ export default {
     },
     // 合计的方法
     getSummaries(param) {
-      if (typeof this.summaryMethod === "function" && this.summaryMethod) {
+      if (typeof this.summaryMethod === 'function' && this.summaryMethod) {
         return this.summaryMethod(param);
       } else {
         const { columns, data } = param;
@@ -432,9 +435,9 @@ export default {
                 return prev;
               }
             }, 0);
-            sums[index] += flas[0].unit || "";
+            sums[index] += flas[0].unit || '';
           } else {
-            sums[index] = "";
+            sums[index] = '';
           }
         });
         return sums;
@@ -443,14 +446,14 @@ export default {
     // 打开自定义字段框
     plDialogOpens() {
       this._times = null;
-      this.$refs.plDialog.style.width = 300 + "px";
+      this.$refs.plDialog.style.width = 300 + 'px';
       this._times = setTimeout(() => {
         this.plDialogFals = true;
       }, 100);
       // 创建节点（主要用来弹出menu窗口时，不让起点击外面）
-      this.aBox = document.createElement("div");
-      this.aBox.className = "modal-backdrop";
-      this.aBox.style.display = "block";
+      this.aBox = document.createElement('div');
+      this.aBox.className = 'modal-backdrop';
+      this.aBox.style.display = 'block';
       this.aBox.onclick = () => {
         this.closeModal();
       };
@@ -460,18 +463,18 @@ export default {
     closeModal() {
       this.plDialogFals = false;
       clearTimeout(this._times);
-      this.$refs.plDialog.style.width = 0 + "px";
-      this.aBox.style.display = "none";
+      this.$refs.plDialog.style.width = 0 + 'px';
+      this.aBox.style.display = 'none';
       this.clearNode();
       this.newDialogData = JSON.parse(JSON.stringify(this.dialogData));
     },
     // 确认按钮
     confirmField() {
-      this.$emit("show-field", this.newDialogData);
+      this.$emit('show-field', this.newDialogData);
       this.plDialogFals = false;
       clearTimeout(this._times);
-      this.$refs.plDialog.style.width = 0 + "px";
-      this.aBox.style.display = "none";
+      this.$refs.plDialog.style.width = 0 + 'px';
+      this.aBox.style.display = 'none';
       this.clearNode();
     },
     // 设置表格高度
@@ -491,7 +494,7 @@ export default {
                 // 表体高
                 if (
                   item.className &&
-                  item.className.indexOf("el-table__body-wrapper") !== -1
+                  item.className.indexOf('el-table__body-wrapper') !== -1
                 ) {
                   if (parseInt(item.style.height)) {
                     if (item.children[0]) {
@@ -526,7 +529,7 @@ export default {
               });
             });
           }
-          this.$emit("load-complete", this.$refs.singleTable);
+          this.$emit('load-complete', this.$refs.singleTable);
         });
       }
     },
@@ -535,13 +538,13 @@ export default {
       if (this.highlightCurrentRow) {
         this.$refs.singleTable.setCurrentRow(row);
       }
-      this.$emit("row-click", row, column, event);
+      this.$emit('row-click', row, column, event);
       // 如果当前节点存在current-row，再次点击时候那么就去掉current-row效果
       if (event.path && this.highlightCurrentRow) {
         event.path.forEach(item => {
           if (item && item.classList) {
             item.classList.forEach(its => {
-              if (its === "current-row") {
+              if (its === 'current-row') {
                 this.$refs.singleTable.setCurrentRow();
               }
             });
@@ -551,7 +554,7 @@ export default {
     },
     // 当某一行被双点击时
     rowDblclick(row, column, event) {
-      this.$emit("row-dblclick", row, column, event);
+      this.$emit('row-dblclick', row, column, event);
     },
     // 用于单选表格，设定某一行为选中行
     setCurrentRow(row) {
@@ -563,31 +566,31 @@ export default {
     },
     // 当用户对某一行展开或者关闭的时候会触发该事件
     expChang(row, expandedRows) {
-      this.$emit("expand-change", row, expandedRows);
+      this.$emit('expand-change', row, expandedRows);
     },
     // 当某一列的表头被点击时会触发该事件
     headerClick(column, event) {
-      this.$emit("header-click", column, event);
+      this.$emit('header-click', column, event);
     },
     // 修改字段名按钮事件
     amendField(item, index) {
-      this.$emit("amend-field", item, index);
+      this.$emit('amend-field', item, index);
     },
     // 表体滚动事件
     tableBodyScroll(table, event) {
       // 当表格滚动暴露滚动事件
-      this.$emit("table-body-scroll", table, event);
+      this.$emit('table-body-scroll', table, event);
     },
     // 当拖动表头改变了列的宽度的时候会触发该事件(newWidth, oldWidth, column, event)
     headerDragend(newWidth, oldWidth, column, event) {
-      this.$emit("header-dragend", newWidth, oldWidth, column, event);
+      this.$emit('header-dragend', newWidth, oldWidth, column, event);
     },
     // 删除节点
     clearNode() {
       // 删除节点
-      let parent = this.aBox ? this.aBox.parentNode : "";
+      let parent = this.aBox ? this.aBox.parentNode : '';
       parent && parent.removeChild(this.aBox);
-      let doms = document.getElementsByClassName("modal-backdrop");
+      let doms = document.getElementsByClassName('modal-backdrop');
       if (doms.length > 0) {
         document.body.removeChild(doms[0]);
       }
